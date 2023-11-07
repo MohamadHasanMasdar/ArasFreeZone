d5badbdc80a2e759ee76707537cc122f1fd17adf
m.h.masdar1995@gmail.com <m.h.masdar1995@gmail.com>
2023-11-07 19:55:37 +0330

    Initial commit

---
+++ app/src/main/java/com/example/arka/arasfreezone1/favoriteActivity.java
@@ -0,0 +1,36 @@
+package com.example.arka.arasfreezone1;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import com.example.arka.arasfreezone1.fragments.favoriteFragment;
+
+public class favoriteActivity extends AppCompatActivity {
+
+    private RelativeLayout container;
+    private FragmentTransaction ft;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+        initView();
+
+
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container1, new favoriteFragment());
+        ft.commit();
+    }
+
+    private void initView() {
+        container = (RelativeLayout) findViewById(R.id.container1);
+    }
+
+        @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.stay, R.anim.activity_back_enter);
+    }
+}
